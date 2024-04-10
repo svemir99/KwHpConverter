65e4d54b7cd9fe985ce0c46c9727096a68e9e7b4
ACER PATOFNA <ACER PATOFNA@DESKTOP-457END1>
2024-04-10 11:56:27 +0200

    Add project files.

---
+++ KwHpConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double result = 0;
+
+            Console.WriteLine("Please, enter value: ");
+            double value = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Convert to (enter kw or hp):");
+            string convertTo = Console.ReadLine();
+
+            if (convertTo == "kw")
+            {
+                result = KwToHp(value);
+            }
+            else
+            {
+                result = HpToKw(value);
+            }
+
+            Console.WriteLine(result);
+
+        }
+
+        private static double KwToHp(double kw)
+        {
+            return kw / 0.745699872;
+        }
+
+        private static double HpToKw(double hp)
+        {
+            return hp * 0.745699872;
+        }
+
+    }
+
+}
